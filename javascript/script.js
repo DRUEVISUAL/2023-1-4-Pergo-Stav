651f448fc553a0499baff42d7da43e47a10706db
András Dobos <druevisual@gmail.com>
2023-01-29 17:23:29 +0100

    added animation to produkty

--- javascript/script.js
+++ javascript/script.js
@@ -5,3 +5,27 @@ menuButton.addEventListener("click", ()=>{
     menu.classList.toggle("menu_active")
 })
 
+////////////////////////////////////////////////////////////
+
+const category = document.querySelectorAll(".cat_con")
+const title = document.querySelector(".title")
+const desc = document.querySelector(".category_description")
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        entry.target.classList.toggle("cat_con_visible", entry.isIntersecting)
+        if (entry.isIntersecting) observer.unobserve(entry.target)
+        if (entry.isIntersecting) console.log("working")
+    })
+}, {
+    rootMargin: "32px"
+})
+
+
+category.forEach(card =>{
+    observer.observe(card)
+})
+
+
+observer.observe(document.querySelector(".title"))
+observer.observe(document.querySelector(".category_description"))
