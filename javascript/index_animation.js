ab1f6c8a40a4d68bff7fb6e60dfdb4cde17f468c
András Dobos <druevisual@gmail.com>
2023-01-30 01:02:38 +0100

    Update index_animation.js

--- javascript/index_animation.js
+++ javascript/index_animation.js
@@ -11,14 +11,21 @@ gsap.from(".title", {
   });
 
   gsap.from(".category", {
-    scrollTrigger: ".category", x: "-45%", ease: 'ease-in', opacity: '0', delay: ".25", filter: 'blur(1px)', stagger: '.1'
+    scrollTrigger: ".category", x: "-45%", ease: 'ease-in', opacity: '0', delay: ".6", filter: 'blur(1px)', stagger: '.1'
   });
 
-gsap.from('.hb', { druation: '3s', x: '-100%', ease: 'ease', filter: 'blur(20px)', delay: '.25'})
+gsap.from('.hb', { druation: '4s', x: '-100%', ease: 'ease', filter: 'blur(20px)', delay: '.25'})
+
 gsap.from('.hero_desktop_column', { druation: '1', x: '-200%', ease: 'ease-in', opacity: '0', filter: 'blur(20px)' })
+
 gsap.from('.hero_desktop_bottom_card', { druation: '.5', x: '-100%', ease: 'ease-in', opacity: '0' })
-gsap.from('.hero_nav', { druation: '.15', x: '-50%', ease: 'ease-in', opacity: '0', delay: '.25'})
+
+gsap.from('.hero_nav', { druation: '.15', x: '-75%', ease: 'ease-in', opacity: '0', delay: '.25', filter: 'blur(2px)'})
+
 gsap.from('.logo', { druation: '.25', x: '-100%', ease: 'ease-in', opacity: '0', delay: '.25' })
-gsap.from('.hero_title_container', { druation: '.5', x: '-100%', ease: 'ease-in', opacity: '0', delay: '.50'})
-gsap.from('.hero_desktop_bottom_card p', { druation: '.35', x: '-100%', ease: 'ease-in', opacity: '0', delay: '.65' })
-gsap.from('.hero_bottom', { druation: '.65', x: '-75%', ease: 'ease-in', opacity: '0', delay: '.80' })
+
+gsap.from('.hero_title_container', { druation: '.5', x: '-100%', ease: 'ease-in', opacity: '0', delay: '.50', filter: 'blur(4px)'})
+
+gsap.from('.hero_desktop_bottom_card p', { druation: '.35', x: '-100%', ease: 'ease-in', opacity: '0', delay: '.65' , filter: 'blur(4px)'})
+
+gsap.from('.hero_bottom', { druation: '3', ease: 'ease-in', opacity: '0', delay: '1.6' })
